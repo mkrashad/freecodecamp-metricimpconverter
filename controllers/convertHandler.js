fd553e67467bb83c96439a2346652b29cab44e7f
Rashad Mahmudov <mkrashad25@gmail.com>
2023-10-26 16:31:38 +0200

    WIP

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,42 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
+  this.getNum = function (input) {
+    const pattern = /[0-9]\.[0-9]/g;
+    const result = input.match(pattern);
+    return Number(result[0]);
   };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
+
+  this.getUnit = function (input) {
+    const pattern = /[a-z]/gi;
+    const result = input.match(pattern).join('');
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
+  this.getReturnUnit = function (initUnit) {
     let result;
-    
+
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.spellOutUnit = function (unit) {
+    let result;
+
+    return result;
+  };
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     let result;
-    
+
     return result;
   };
-  
 }
 
 module.exports = ConvertHandler;
--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,12 @@ const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
 
+  app.route('/api/convert').get(function (req, res) {
+    const initNum = convertHandler.getNum(req.query.input);
+    const initUnit = convertHandler.getUnit(req.query.input);
+    console.log(initUnit);
+    res.status(200).json({ initNum, initUnit });
+  });
 };
